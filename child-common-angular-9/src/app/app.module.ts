5f5be9dbf4362ca68518bda11316aef689c7380a
GQ <tonny008@163.com>
2020-07-10 16:47:25 +0800

    change Angular app bootstrap to use getMountNode()

--- child-common-angular-9/src/app/app.module.ts
+++ child-common-angular-9/src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, ApplicationRef } from "@angular/core";
+import {
+  isInIcestark,
+  getMountNode,
+  registerAppEnter,
+  registerAppLeave,
+} from "@ice/stark-app";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  ngDoBootstrap(appRef: ApplicationRef) {
+    if (isInIcestark()) {
+      const ele = getMountNode();
+      appRef.bootstrap(AppComponent, ele);
+    } else {
+      appRef.bootstrap(AppComponent);
+    }
+  }
+}
